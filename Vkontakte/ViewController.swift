b60bf08062ad67cc1980aa39c04751e9fa023ba8
Valerii El-Khatib <elkhatibvm@gmail.com>
2019-10-15 04:56:23 +0300

    Initial Commit

---
+++ Vkontakte/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vkontakte
+//
+//  Created by Валерий Эль-Хатиб on 15.10.2019.
+//  Copyright © 2019 EVM Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VkontakteTests/VkontakteTests.swift
@@ -0,0 +1,34 @@
+//
+//  VkontakteTests.swift
+//  VkontakteTests
+//
+//  Created by Валерий Эль-Хатиб on 15.10.2019.
+//  Copyright © 2019 EVM Corporation. All rights reserved.
+//
+
+import XCTest
+@testable import Vkontakte
+
+class VkontakteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
