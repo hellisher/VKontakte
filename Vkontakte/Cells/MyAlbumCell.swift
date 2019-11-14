946a413b4cc1cfd9c004993f06c85e683128875b
Valerii El-Khatib <elkhatibvm@gmail.com>
2019-11-14 14:21:55 +0300

    Lesson5. Realm added

--- Vkontakte/Cells/MyAlbumCell.swift
+++ Vkontakte/Cells/MyAlbumCell.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class MyAlbumCell: UICollectionViewCell {
     
-    @IBOutlet weak var friendPhoto: UIImageView!
+    @IBOutlet weak var userPhotos: UIImageView!
     
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
 }
--- Vkontakte/ViewControllers/MyAlbumController.swift
+++ Vkontakte/ViewControllers/MyAlbumController.swift
@@ -8,50 +8,34 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
-class AlbumController: UICollectionViewController {
-
+class MyAlbumController: UICollectionViewController {
+    
+    var userPhotos = [UserPhoto]()
+    var api = GetVKAPI()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        api.loadUserPhotosData { photos in
+            self.userPhotos = photos
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
-
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
         return 1
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyAlbumCell", for: indexPath) as! MyAlbumCell
+//        let userPhoto = userPhotos[indexPath.row]
+//        cell.userPhotos = userPhoto
         return cell
     }
 
