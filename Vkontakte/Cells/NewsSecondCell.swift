a677c52607272070b75742dbd32fcf6f91e9ec0a
Valerii El-Khatib <elkhatibvm@gmail.com>
2019-12-28 21:13:09 +0300

    Commit 28 12 19

---
+++ Vkontakte/Cells/NewsSecondCell.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class NewsSecondCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
